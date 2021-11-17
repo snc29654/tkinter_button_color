1b3ae8831724ccf2cf110dba2091d86b4672227d
snc29654 <snc29654@nifty.com>
2021-11-17 10:11:05 +0900

    ファイル選択メニュー追加

--- button_jpg_back.py
+++ button_jpg_back.py
@@ -1,6 +1,30 @@
 import tkinter as tk
 from tkinter import ttk
 from PIL import ImageTk, Image
+import tkinter
+import glob
+from tkinter import *
+from PIL import ImageTk, Image
+import os
+import sys
+import time
+import tkinter
+from PIL import Image, ImageTk
+import threading
+import time
+import glob
+import os,sys
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import os
+import glob
+from tkinter import filedialog as tkFileDialog
+import tkinter as tk
+from tkinter import font
+from tkinter.scrolledtext import ScrolledText
 
 
 
@@ -14,18 +38,48 @@ class color_button(ttk.Combobox):
         self.color="Red"
         li = ['Red', 'Blue', 'Yellow','Green', 'Black', 'White',
               'Gray', 'ivory', 'pink','violet', 'lawngreen', 'gold',     
-              'cyan', 'lightgray']     
+              'cyan', 'lightgray','brown']     
         super().__init__(master, values=li) 
+        
+        self.filenames ="WS000232.JPG"
+        
+
+        button3= Button(root, text=u'ファイル選択', font=24,command=self.button3_clicked)  
+        button3.grid(row=0, column=1)  
+        button3.place(x=950, y=70) 
+        
+    
+        
+        
+        
         self.var = var                      
         self.bind(                          
             '<<ComboboxSelected>>',
             self.show_selected
             )
+
+
+    def button3_clicked(self):  
+
+        fTyp = [('', '*')] 
+        iDir = os.path.abspath(os.path.dirname(__file__)) 
+        self.filenames = tkFileDialog.askopenfilenames(filetypes= [("JPEG", ".jpg")], initialdir=iDir)
+
+        if(self.filenames != ""):
+            button4= Button(root, text=u'表示', font=24,command=self.button4_clicked,bg='#f0e68c')  
+            button4.grid(row=0, column=1)  
+            button4.place(x=950, y=100) 
+
+
+    def button4_clicked(self):  
+        self.setnumber()
+
+
     def show_selected(self, event):
         self.color = self.get()        
     def setnumber(self):
-
-        img2 = Image.open("WS000232.jpg")
+        for file in self.filenames:
+            img2 = Image.open(file)
         image2 = img2.convert('RGB')
         width, height = image2.size
 
@@ -52,7 +106,6 @@ class color_button(ttk.Combobox):
                 btn.grid(column=column, row=row)
                 #print(self.from_rgb_to_colorcode((self.r, self.g, self.b)))
                 btn.config(command=self.collback(btn),bg=self.from_rgb_to_colorcode((self.r, self.g, self.b)))
-        root.mainloop()
 
     def collback(self,btn):
         def nothing():
@@ -76,6 +129,6 @@ l = tk.Label(textvariable=var,font=48)
 l.place(x=700,y=750)
 c=color_button(master=root, var=var)
 c.place(x=950,y=0)
-c.setnumber()
+root.mainloop()
 
 
